3d3ca6d4ea25b06048ab34592210eeb2546789b4
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2020-06-19 19:31:14 -0500

    first commit

---
+++ resources/views/emails/themes-clients/newsletter.blade.php
@@ -0,0 +1,5 @@
+@component('mail::message')
+<h1 style="text-align: center;">Cordial saludo, gracias por suscribirte a nuestro boletín</h1>
+<img class="img-responsive" src="{{ url('/img/logo_bmotion_dark.jpg') }}" style="padding:15%">
+<p style="text-align: center;">Te notificaremos cuando estemos listos.</p>
+@endcomponent
